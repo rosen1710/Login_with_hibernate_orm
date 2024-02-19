d74bd19ce5892fedf6ba5d8ecb953773fd3895fd
rosen <rosen1710@abv.bg>
2024-02-19 20:25:48 +0200

    Third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,7 @@
 package org.example;
 
-import javax.naming.AuthenticationException;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        User user;
-
-        // Create permissions
-        Permission readPaymentsPermission = new Permission("payments-read");
-        Permission writeDBPermission = new Permission("db-write");
-
-        // Create a group and associate permissions
-        Group group = new Group(Arrays.asList(readPaymentsPermission, writeDBPermission));
-
-        // Create a user and associate the group
-        user = new User("userName", "MyPassword", Arrays.asList(readPaymentsPermission, writeDBPermission), List.of(group));
-
-        AccountsManager.addPermission(readPaymentsPermission);
-        AccountsManager.addPermission(writeDBPermission);
-        AccountsManager.addGroup(group);
-        AccountsManager.registerUser(user);
-
-        System.out.println("Entities saved successfully!");
-
-        try {
-            user = AccountsManager.getUser("userName", "MyPassword");
-        } catch (AuthenticationException e) {
-            System.out.println(e.getMessage());
-            return;
-        }
-        System.out.println(user);
+        System.out.println("Hello world!");
     }
 }
---
+++ src/test/java/AccountsManagerTest.java
@@ -0,0 +1,43 @@
+import org.example.AccountsManager;
+import org.example.Group;
+import org.example.Permission;
+import org.example.User;
+import org.junit.jupiter.api.Test;
+
+import javax.naming.AuthenticationException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AccountsManagerTest {
+    @Test
+    public void testAddUser() {
+        // Create permissions
+        Permission readPaymentsPermission = new Permission("payments-read");
+        Permission writeDBPermission = new Permission("db-write");
+
+        // Create a group and associate permissions
+        Group group = new Group(Arrays.asList(readPaymentsPermission, writeDBPermission));
+
+        // Create a user and associate the group
+        User user = new User("userName", "MyPassword", Arrays.asList(readPaymentsPermission, writeDBPermission), List.of(group));
+
+        AccountsManager.addPermission(readPaymentsPermission);
+        AccountsManager.addPermission(writeDBPermission);
+        AccountsManager.addGroup(group);
+        AccountsManager.registerUser(user);
+
+        System.out.println("Entities saved successfully!");
+    }
+
+    @Test
+    public void testGetUser() {
+        User user;
+        try {
+            user = AccountsManager.getUser("userName", "MyPassword");
+        } catch (AuthenticationException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        System.out.println(user);
+    }
+}
